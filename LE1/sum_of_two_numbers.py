3dde500ba0969b7c0769ce458ee088e6f078b297
231B349 <231B349@juet.co.in>
2025-07-29 15:27:57 +0530

    new python file update

---
+++ LE1/sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Add two numbers
+
+# Input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Print the result
+print("The sum is:", sum)
